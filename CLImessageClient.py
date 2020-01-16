9abf76135e5d3b373d1c87aee864878fc895f3a6
OscarVera <oscar.vera@atomic32.com>
2020-01-16 10:43:06 -0600

    real oop on python, kinda..

--- CLImessageClient.py
+++ CLImessageClient.py
@@ -1,8 +1,11 @@
 import os
 
-class CLImessageClient:
+class CLImessageClient(object):
     pathLanguages = os.path.join("languages")
     option = ""
+
+    def __init__(self, ):
+        print("")
 
     def readfile(self, language):
         path = os.path.join("languages", language, "LC_MESSAGES", "messages.po")
@@ -20,10 +23,24 @@ class CLImessageClient:
     def toList(self, message):
         messageList = message.split("\n\n")
         messageMatrix = [[0 for x in range(3)] for y in range(len(messageList))]
-        line = messageList[0]
-        messageMatrix[0][0] = line[:line.index("\nmsgid")]
-        messageMatrix[0][1] = line[line.index("ms"):line.index("\nmsgstr")]
-        messageMatrix[0][2] = line[line.index("msgstr"):]
+        a = 0
+        for x in range(len(messageList)):
+            line = ""
+            try:
+                if x == 0:
+                    line = messageList[x]
+                    messageMatrix[0][0] = line[:line.index("\nmsgid")]
+                    messageMatrix[0][1] = line[line.index("ms"):line.index("\nmsgstr")]
+                    messageMatrix[0][2] = line[line.index("msgstr"):]
+                else: 
+                    line = messageList[x]
+                    messageMatrix[x][0] = line[:line.index("\nmsgid")]
+                    messageMatrix[x][1] = line[line.index("msgid"):line.index("\nmsgstr")]
+                    messageMatrix[x][2] = line[line.index("msgstr"):]
+            except Exception:
+                print(a)
+                print(line)
+            a = a +1 
         return messageMatrix
 
     def message (self):
@@ -34,4 +51,5 @@ class CLImessageClient:
 
 
 run = CLImessageClient()
-print(run.toList(run.readfile("es")))
+run.toList(run.readfile("es"))
+#print(run.toList(run.readfile("es")))
