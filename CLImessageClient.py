29636c971f33e5305ad058d62c07b2a4607363e4
OscarVera <oscar.vera@atomic32.com>
2020-01-22 14:10:13 -0600

    22 enero, problemas con textToDic

--- CLImessageClient.py
+++ CLImessageClient.py
@@ -18,50 +18,54 @@ class CLImessageClient(object):
             print("ok")
         return language
 
-    def dictionaryToText(self, messages):
-        messagesDictionary = messages
-        #for x in range(len(messagesDictionary)):
-                
-        pass
+    def dictionaryToText(self, messagesDictionary):
+        messagesDictionary = messagesDictionary
+        idsList = sorted(messagesDictionary)
+        repeatedIdsList = []
+        fuzzyIdsList = []
+        newString = ""
+        repeatedString = ""
+        fuzzyString = ""
+        for x in range(len(idsList)):
+            if idsList[x] == "//Repeated":
+                print(messagesDictionary[idsList[x]])
+                repeatedIdsList = sorted(messagesDictionary[idsList[x]])
+                for y in range(len(repeatedIdsList)):
+                    if messagesDictionary[idsList[x]][repeatedIdsList[y]]["Comments"] != "":
+                        repeatedString += messagesDictionary[idsList[x]][repeatedIdsList[y]]["Comments"] + "\n"
+                    repeatedString += "msgid \""
+                    repeatedString += repeatedIdsList[y] + "\"\n"
+                    repeatedString += "msgstr \""
+                    repeatedString += messagesDictionary[idsList[x]][repeatedIdsList[y]]["msgstr"] + "\"\n\n"
+            elif idsList[x] == "//Fuzzy":
+                print(messagesDictionary[idsList[x]])
+                fuzzyIdsList = sorted(messagesDictionary[idsList[x]])
+                for y in range(len(fuzzyIdsList)):
+                    if messagesDictionary[idsList[x]][fuzzyIdsList[y]]["Comments"] != "":
+                        fuzzyString += messagesDictionary[idsList[x]][fuzzyIdsList[y]]["Comments"] + "\n"
+                    fuzzyString += "#~ msgid \""
+                    fuzzyString += fuzzyIdsList[y] + "\"\n"
+                    fuzzyString += "#~ msgstr \""
+                    fuzzyString += messagesDictionary[idsList[x]][fuzzyIdsList[y]]["msgstr"] + "\"\n\n"
+            else:
+                if messagesDictionary[idsList[x]]["Comments"] != "":
+                    newString += messagesDictionary[idsList[x]]["Comments"] + "\n"
+                newString += "msgid \""
+                newString += idsList[x] + "\"\n"
+                newString += "msgstr \""
+                newString += messagesDictionary[idsList[x]]["msgstr"] + "\"\n\n"
+        newString += repeatedString
+        newString += fuzzyString
+        print(newString)
+        return newString
 
-    def addMessage(self, message, language):
-        errors = 1
-        while errors == 1:
-            try:
-                newMsgid = raw_input("Write a message (a message in English that can be translated): ")
-                if any(char.isdigit() for char in newMsgid):
-                    raise Exception("No numbers")
-                errors = 0
-            except:
-                print("Message id should not contain numbers")
-        errors = 1
-        while errors == 1:
-            try:
-                newMsgstr = raw_input("Write the translation of the message on " + language + ": ")
-                if any(char.isdigit() for char in newMsgstr):
-                    raise Exception("No numbers")
-                errors = 0
-            except:
-                print("Message translation should not contain numbers")
-        while errors == 1:
-            try:
-                newCommentsLine = raw_input("Write a comment on where it's gonna be used (can be left blank):")
-                errors = 0
-            except:
-                print("Error on Comment")   
-        if newCommentsLine != "":
-            newCommentsLine = "#" + newCommentsLine
-        if newMsgid in message:
-            print("That message id already exists")
-        else:  
-            message[newMsgid] = {"Comments" : newCommentsLine, "msgstr" : newMsgstr}
-            return message
-
-    def dictionaryGeneration(self, message):
+    def textToDictionary(self, message):
         messageList = message.split("\n\n")
         messageDic = {}
         repeatedDic = {}
+        fuzzyDic = {}
         a = 0
+        #n = 0
         for x in range(len(messageList)):
             a = a +1
             wholeLine = ""
@@ -94,13 +98,17 @@ class CLImessageClient(object):
                     msgstr = msgstrLine[msgstrLine.index("\"")+1:-1]
             except Exception:
                 print("error inesperado en" + wholeLine)
-            if msgid in messageDic:
-                if "#~" in msgidLine:
-                    index = message.index(msgidLine[3:])
-                    secondIndex = message.index(msgidLine[3:],index + 2)
-                else:
-                    index = message.index(msgidLine)
-                    secondIndex = message.index(msgidLine,index + 2)
+            if "#~" in msgidLine:
+                index = message.index(msgidLine[3:])
+                secondIndex = message.index(msgidLine,index + 2)
+                firstInstance = len(message[:index].split("\n"))
+                secondInstance = len(message[:secondIndex].split("\n"))
+                fuzzyDic[msgid] = {"Comments" : commentsLine, "msgstr" : msgstr, 
+                    "Instance 1" : firstInstance,
+                    "Instance 2" : secondInstance}
+            elif msgid in messageDic:
+                index = message.index(msgidLine)
+                secondIndex = message.index(msgidLine,index + 2)
                 firstInstance = len(message[:index].split("\n"))
                 secondInstance = len(message[:secondIndex].split("\n"))
                 repeatedDic[msgid] = {"Comments" : commentsLine, "msgstr" : msgstr, 
@@ -108,8 +116,62 @@ class CLImessageClient(object):
                     "Instance 2" : secondInstance}
             else:
                 messageDic[msgid] = {"Comments" : commentsLine, "msgstr" : msgstr}
-            self.__repeated = len(repeatedDic)
+        self.__repeated = len(repeatedDic)
+        self.__fuzzy = len(fuzzyDic)
+        print(repeatedDic)
+        print(fuzzyDic)
+        messageDic["//Repeated"] = repeatedDic
+        messageDic["//Fuzzy"] = fuzzyDic
         return messageDic
+
+    def addMessage(self, messageDictionary, language):
+        repeated = 0
+        while repeated == 0:
+            errors = 1
+            while errors == 1:
+                try:
+                    newMsgid = raw_input("Write a message (a message in English that can be translated): ")
+                    if any(char.isdigit() for char in newMsgid):
+                        raise Exception("No numbers")
+                    errors = 0
+                except:
+                    print("Message id should not contain numbers")
+            errors = 1
+            while errors == 1:
+                try:
+                    newMsgstr = raw_input("Write the translation of the message on " + language + ": ")
+                    if any(char.isdigit() for char in newMsgstr):
+                        raise Exception("No numbers")
+                    errors = 0
+                except:
+                    print("Message translation should not contain numbers")
+            errors = 1
+            while errors == 1:
+                try:
+                    newCommentsLine = raw_input("Write a comment on where it's gonna be used (can be left blank):")
+                    errors = 0
+                except:
+                    print("Error on Comment")   
+            if newCommentsLine != "":
+                newCommentsLine = "#" + newCommentsLine
+            if newMsgid in messageDictionary:
+                print("That message id already exists")
+                print("Want to try to add another Message?")
+                errors = 1
+                while errors == 1:
+                    try:
+                        inputRepeated = int(raw_input("Write 0 to try to add another message or 1 to return to the main menu"))
+                        if inputRepeated < 0 or inputRepeated > 1:
+                            raise Exception("Value under 0 or under 1")
+                        repeated = inputRepeated
+                        errors = 0
+                    except: 
+                        print("Only 0 and 1 accepted")
+                        print("")
+            else:  
+                messageDictionary[newMsgid] = {"Comments" : newCommentsLine, "msgstr" : newMsgstr}
+                repeated = 1
+        return messageDictionary
 
 class Menu(object):
     def __init__(self):
@@ -206,11 +268,11 @@ class Runner(CLImessageClient,Menu,PathHandler,SpecificRecord,FileManager):
     option = 0
     optionLan = 0
     __message = ""
-    repeated = 0
     __messageDictionary = {}
+    __repeatedDic = {}
+    __repeated = 0
 
     def __init__(self):
-
         self.optionLan = 1
         self.runApp()
 
@@ -238,16 +300,18 @@ class Runner(CLImessageClient,Menu,PathHandler,SpecificRecord,FileManager):
                     self.language = self.__listLanguages[self.optionLan -1]
                     self.__pathCatalog = self.buildPath(self.language)
                     self.__pathMessages = self.buildPathMessages(self.language)
+                    self.__repeated = 0
+                    self.__repeatedDic = {}
                     self.__messageDictionary = {}
                     self.__message = ""
                     self.showMenu(self.language)
-                    
+                    print("")
                     if self.option == 1:
                         self.__pathCatalog
                         self.__message = self.readfile(self.language, self.__pathMessages)
-                        self.__messageDictionary = self.dictionaryGeneration(self.__message)
-                        self.__message = self.addMessage(self.__message, self.language)
-                        #to text
+                        self.__messageDictionary = self.textToDictionary(self.__message)
+                        self.__messageDictionary = self.addMessage(self.__messageDictionary, self.language)
+                        self.__message = self.dictionaryToText(self.__messageDictionary)
                         #check if correct
                         self.compile(self.__pathCatalog)
                     elif self.option == 2:
@@ -255,9 +319,8 @@ class Runner(CLImessageClient,Menu,PathHandler,SpecificRecord,FileManager):
                     elif self.option == 3:
                         self.__pathCatalog
                         self.__message = self.readfile(self.language, self.__pathMessages)
-                        self.__messageDictionary = self.dictionaryGeneration(self.__message)
+                        self.__messageDictionary = self.textToDictionary(self.__messageDictionary)
                         print(self.__messageDictionary)
-
                     elif self.option == 4:
                         pass
                     elif self.option == 5:
